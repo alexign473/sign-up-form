1dfcf4ff72b82a0c49111519923d6b3aa5c82cd2
Alexander Ignatev <alexign473@gmail.com>
2022-12-27 05:29:14 +0300

    Refactor, remove index

--- main.js
+++ main.js
@@ -1,60 +1,62 @@
 const select = (selector) => document.querySelector(selector);
-const classes = (classes) => document.getElementsByClassName(classes);
 
 const firstName = select('#first-name'),
   email = select('#email'),
   password = select('#password'),
   confirmPassword = select('#confirm-password'),
-  form = select('#sign-up-form'),
-  errorMsg = classes('error'),
-  successIcon = classes('success-icon'),
-  failureIcon = classes('failure-icon');
+  form = select('#sign-up-form');
 
-const showError = (input, index, message) => {
-  errorMsg[index].textContent = message;
+const showError = (input, message) => {
+  const formGroup = input.parentElement;
+  const errorContainer = formGroup.querySelector('.error');
+  const errorIcon = formGroup.querySelector('.error-icon');
+  const successIcon = formGroup.querySelector('.success-icon');
+
+  errorContainer.textContent = message;
   input.style.border = '2px solid var(--c-error)';
-  // icons
-  failureIcon[index].style.opacity = '1';
-  successIcon[index].style.opacity = '0';
+  errorIcon.style.opacity = '1';
+  successIcon.style.opacity = '0';
 };
 
-const showSuccess = (input, index) => {
-  errorMsg[index].textContent = '';
+const showSuccess = (input) => {
+  const formGroup = input.parentElement;
+  const errorContainer = formGroup.querySelector('.error');
+  const errorIcon = formGroup.querySelector('.error-icon');
+  const successIcon = formGroup.querySelector('.success-icon');
+
+  errorContainer.textContent = '';
   input.style.border = '2px solid green';
-  // icons
-  failureIcon[index].style.opacity = '0';
-  successIcon[index].style.opacity = '1';
+  errorIcon.style.opacity = '0';
+  successIcon.style.opacity = '1';
 };
 
-const checkRequired = (input, index, message) => {
+const checkRequired = (input, message) => {
   if (input.value.trim() === '') {
-    showError(input, index, message);
+    showError(input, message);
     return false;
   } else {
-    showSuccess(input, index);
+    showSuccess(input);
     return true;
   }
 };
 
-const checkEmail = (email, index) => {
-  if (!checkRequired(email, index, 'Please enter an email address'))
-    return false;
+const checkEmail = (email) => {
+  if (!checkRequired(email, 'Please enter an email address')) return false;
 
   if (!email.checkValidity()) {
-    showError(email, index, 'Please enter a valid email');
+    showError(email, 'Please enter a valid email');
     return false;
   } else {
     return true;
   }
 };
 
-const checkPassword = (password, index) => {
-  if (!checkRequired(password, index, 'Please enter a password')) return false;
+const checkPassword = (password) => {
+  if (!checkRequired(password, 'Please enter a password')) return false;
 
   if (!isPasswordSecure(password.value.trim())) {
     showError(
       password,
-      index,
       'Password must has at least 8 characters that include at least 1 uppercase character, 1 lowercase character and 1 number'
     );
     return false;
@@ -63,27 +65,24 @@ const checkPassword = (password, index) => {
   }
 };
 
-const checkConfirmPassword = (confirmPassword, index) => {
-  if (!checkRequired(confirmPassword, index, 'Please confirm your password'))
+const checkConfirmPassword = (password, confirmPassword) => {
+  if (!checkRequired(confirmPassword, 'Please confirm your password'))
     return false;
+
   if (password.value.trim() !== confirmPassword.value.trim()) {
-    showError(confirmPassword, index, 'Passwords do not match');
+    showError(confirmPassword, 'Passwords do not match');
     return false;
   } else {
     return true;
   }
 };
 
-const checkPhone = (phone, index) => {
+const checkPhone = (phone) => {
   if (!isPhoneNumbers(+phone.value.trim())) {
-    showError(
-      phone,
-      index,
-      'The phone number must be a 10 digit or less number'
-    );
+    showError(phone, 'The phone number must be a 10 digit or less number');
     return false;
   } else {
-    showSuccess(phone, index);
+    showSuccess(phone);
     return true;
   }
 };
@@ -95,7 +94,7 @@ const isPasswordSecure = (password) => {
 
 const isPhoneNumbers = (number) => {
   const re = /^[0-9]{10}$/;
-  return re.test(number);
+  return re.test(+number);
 };
 
 const debounce = (fn, delay = 500) => {
@@ -115,14 +114,13 @@ const debounce = (fn, delay = 500) => {
 const submitForm = (e) => {
   e.preventDefault();
 
-  const isFirstNameValid = checkRequired(
-    firstName,
-    0,
-    'Please enter your name'
+  const isFirstNameValid = checkRequired(firstName, 'Please enter your name');
+  const isEmailValid = checkEmail(email);
+  const isPasswordValid = checkPassword(password);
+  const isConfirmPasswordValid = checkConfirmPassword(
+    password,
+    confirmPassword
   );
-  const isEmailValid = checkEmail(email, 2);
-  const isPasswordValid = checkPassword(password, 4);
-  const isConfirmPasswordValid = checkConfirmPassword(confirmPassword, 5);
 
   const isFormValid =
     isFirstNameValid &&
@@ -130,30 +128,25 @@ const submitForm = (e) => {
     isPasswordValid &&
     isConfirmPasswordValid;
 
-  if (!isFormValid) {
-    e.preventDefault();
-    console.log('not valid');
-  } else {
-    console.log('valid');
-  }
+  if (isFormValid) alert('Success');
 };
 
 const validateInput = (e) => {
   switch (e.target.id) {
     case 'first-name':
-      checkRequired(firstName, 0, 'Please enter your name');
+      checkRequired(firstName, 'Please enter your name');
       break;
     case 'email':
-      checkEmail(email, 2);
+      checkEmail(email);
       break;
     case 'phone':
-      checkPhone(phone, 3);
+      checkPhone(phone);
       break;
     case 'password':
-      checkPassword(password, 4);
+      checkPassword(password);
       break;
     case 'confirm-password':
-      checkConfirmPassword(confirmPassword, 5);
+      checkConfirmPassword(password, confirmPassword);
       break;
   }
 };
